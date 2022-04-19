61034538ec09c6171aff9645ac66fb0892ae06a7
bennyh960 <bennyh960@gmail.com>
2022-04-20 02:51:56 +0300

    added all directory

---
+++ algo_and_testing/logestCommonPrefix/longestCommonPrefix.easy.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ *
+ * @description {Write a function to find the longest common prefix string amongst an array of strings.
+                If there is no common prefix, return an empty string "".}
+ * 
+ * Example 1 
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ * 
+ *
+ * Example 2
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ *
+ */
+
+const longestCommonPrefix = function (strs) {
+  return 10;
+};
+
+module.exports = longestCommonPrefix;

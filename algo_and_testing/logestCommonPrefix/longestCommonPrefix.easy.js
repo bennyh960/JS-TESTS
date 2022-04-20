f4dfff1f30b7fd45df0d55211093488208ed62d0
bennyh960 <bennyh960@gmail.com>
2022-04-20 10:22:30 +0300

    finished 1

--- algo_and_testing/logestCommonPrefix/longestCommonPrefix.easy.js
+++ algo_and_testing/logestCommonPrefix/longestCommonPrefix.easy.js
@@ -17,7 +17,32 @@
  */
 
 const longestCommonPrefix = function (strs) {
-  return 10;
+  // find shortest word
+  strs.sort();
+  const shortestLength = strs[0].length;
+
+  // define length variable
+  const length = strs.length;
+
+  let idx = 0;
+  for (let i = 0; i < shortestLength; i++) {
+    if (strs[0][i] === strs[length - 1][i]) {
+      for (let j = 1; j < length - 1; j++) {
+        if (strs[0][i] === strs[j][i]) idx++;
+      }
+    }
+  }
+
+  if (idx === 0) {
+    return "";
+  }
+
+  let prefix = strs[0].substring(0, idx);
+  return prefix;
 };
+
+// console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+// console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+// console.log(longestCommonPrefix(["a", "a", "a"]));
 
 module.exports = longestCommonPrefix;
